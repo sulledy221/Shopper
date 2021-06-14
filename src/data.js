46fba94b610b46922a605a9920bbc4765ae7469e
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-06-14 12:54:32 -0700

    code push

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'This Drawing.',
+        price: 9001,
+        image: 'https://dthezntil550i.cloudfront.net/kg/latest/kg1801080806394750004019000/1280_960/3d3776e3-9d4f-463c-afcc-89ffbf69be31.png',
+      },
+      {
+        id: '2',
+        name: 'Mr. Roboto',
+        price: 8008,
+        image: 'https://i.vimeocdn.com/video/647358865_820.jpg',
+      },
+      {
+        id: '3',
+        name: 'Chimpken Numget',
+        price: 4,
+        image: 'https://static.toiimg.com/thumb/msid-78702801,width-800,height-600,resizemode-75,imgsize-357397,pt-32,y_pad-40/78702801.jpg',
+      },
+    ],
+  };
+  export default data;
